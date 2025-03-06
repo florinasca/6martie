514efe4ad9c24907e753146f91714d6549737fa7
florin <florinasca@mail.ru>
2025-03-06 22:10:52 +0200

    Add commit,

---
+++ src/Telefon.java
@@ -0,0 +1,19 @@
+
+
+public class Telefon {
+    public static void main(String[] args) {
+
+        int number = 100;
+        String model = "nokia";
+        int weight = 200;
+
+        Telefon obiectNumber = new Telefon();
+
+
+    }
+
+    class Phone {
+
+        
+    }
+}
